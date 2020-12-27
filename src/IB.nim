de2a032b161567694aee2adab39015affef8c472
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2020-12-27 21:16:04 +0500

    reading files

--- src/IB.nim
+++ src/IB.nim
@@ -1,1 +1,25 @@
-echo "hello"
+proc start(arr1, arr2: seq[int]) =
+  echo """
+
+1 - Пересечение
+2 - Слияние
+3 - Исключающее или
+4 - Отрицание 1
+5 - Отрицание 2
+6 - Выход
+7 - Повтор"""
+
+proc fileToV(name: string): seq[int] =
+  var arr: seq[int]
+  var file: string = readFile(name)
+  for i in file:
+    if i == '0':
+      arr.add(0)
+    elif i == '1':
+      arr.add(1)
+  return arr
+
+let arr1: seq[int] = fileToV("i1.txt") 
+let arr2: seq[int] = fileToV("i2.txt")
+
+start(arr1, arr2)
